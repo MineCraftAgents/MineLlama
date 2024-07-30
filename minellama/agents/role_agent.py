452e5728f8198a559cf326849ef2cb54a5a13eaf
wa-taiki <beenabee0707@gmail.com>
2024-07-30 18:53:37 +0900

    APIの問題解決後　補足

--- minellama/agents/role_agent.py
+++ minellama/agents/role_agent.py
@@ -47,27 +47,21 @@ class RoleAgent:
             task = {name:json_dict[name]}
         return task
 
-
-    def next_task(self, todaysgoal, inventory, memory=None):
-        system_prompt = self.role_prompt
-        human_prompt = f"goal: {todaysgoal} Inventory: {inventory} Memory: {memory} What is the next task?"
-        response = self.llm.content(system_prompt, query_str=human_prompt)
-        print(response)
-        extracted_response = self.extract_dict_from_str(response)
-        # print(extracted_response)
-        return extracted_response
-    
-    def make_todaysgoal(self, dream, inventory, memory=None):
+    def make_todaysgoal(self, dream, inventory, memory):
+        print("~~~~~~~~~~make_todaysgoal~~~~~~~~~~~")
         system_prompt = """
         You are an assistant of the role playing of Minecraft game.
-        We regard completing role as getting items set as goal.
-        Your task is translating given role texts into item list as a goal.
+        We regard completing role as getting item set as goal.
+        Your task is translating given role texts into item list(python-dict-type) as a goal.
         Please answer the item list that a player who is given the role will finaly get.
         Each time, I give you information below:
         Role: ... ;This is the role which player will be given.
         Invenotory: {{"ITEM_NAME":COUNT,...}} : Those are items which player has now.
         Memory: [{{"TASK":COUNT}},...] ;Those are the tasks you achieved before.
 
+        Here are what you have to pay attention to.
+        While you are answering, please use correct item name of minecraft. do not use ambiguous name like "fertilizer, animal, food, tool, material".
+        you only have to answer the result. please do not answer other text.
         You must follow the python-dict like format below when you answer:
         {{"ITEM_NAME":COUNT}}
 
@@ -75,9 +69,21 @@ class RoleAgent:
         {{"diamond_sword":3}}
         """
         human_prompt = f"Role: {dream} Inventory: {inventory} Memory: {memory}, what does the player have to get to complete role playing? "
-        response = self.llm.content(system_prompt, query_str=human_prompt)
+        response = self.llm.content(system_prompt, query_str=human_prompt, data_dir="recipe")
         print(response)
         extracted_response = self.extract_dict_from_str(response)
         # print(extracted_response)
         return extracted_response
+    
+    def next_task(self, role, todaysgoal, inventory, memory=None):
+        print("~~~~~~~~~~next task~~~~~~~~~~~")
+        system_prompt = self.role_prompt
+        human_prompt = f"Role: {role} Inventory: {inventory} Memory: {memory} What is the next task?"
+        response = self.llm.content(system_prompt, query_str=human_prompt, data_dir="action")
+        print(response)
+        extracted_response = self.extract_dict_from_str(response)
+        # print(extracted_response)
+        return extracted_response
+    
+
         
