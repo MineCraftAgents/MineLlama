609eb17d5efda6a6b1170e195748e368d217333b
wa-taiki <beenabee0707@gmail.com>
2024-08-09 11:21:37 +0900

    不要なprintを削除

--- minellama/agents/combine_relationgraph.py
+++ minellama/agents/combine_relationgraph.py
@@ -10,9 +10,10 @@ todaysgoal_list = ["stone_pickaxe", "stone_sword", "iron_pickaxe"]
 expect_final_inventory = copy.deepcopy(todaysgoal_list)
 
 #採取に必要になるツールが存在すればそれを調べ上げ、そのツールに対してもレシピ分解を行う関数
-def get_full_dependance(agent, final_inventory):
+def get_full_dependance(final_inventory):
     num = 0
     result = []
+    agent = ra.RecipeAgent()
     # final_inventoryのコピーを作成して処理する
     inventory_copy = final_inventory.copy()
     
@@ -24,12 +25,8 @@ def get_full_dependance(agent, final_inventory):
             for ingredient in r:
                 tool = agent.required_tool(list(ingredient.keys())[0])
                 if tool and tool not in inventory_copy:
-                    print(f"{tool} is added into the final inventory")
-                    inventory_copy.append(tool)
-                    
+                    inventory_copy.append(tool)                    
         num += 1
-    
-    print("---------recipe:",recipe, "--------")
     return recipe
 
 get_full_dependance(agent, todaysgoal_list)
