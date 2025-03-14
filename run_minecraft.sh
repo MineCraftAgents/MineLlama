bef40bf76b86d8436af4667ed759208ab191c936
Shiyao DING <dingshiyao0217@gmail.com>
2025-03-14 18:35:48 +0900

    Set up by Gradle Automation

---
+++ run_minecraft.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+# Navigate to the forge-1 directory
+cd /Users/drp2024/Desktop/forge-1
+
+# Add Gradle to the PATH
+export PATH=$PATH:/opt/gradle/gradle-8.8/bin
+
+# Display Gradle version
+gradle -v
+
+# Build the project
+gradle build
+
+# Run the Minecraft client
+gradle runclient
