650d26cc34f375b3bb406894e30d79ae61a0c00d
wa-taiki <165888638+wa-taiki@users.noreply.github.com>
2025-04-25 13:23:43 +0900

    アイテムの自動検索を行う際もLLMの数が１つでよいように変更

--- minellama/minellama.py
+++ minellama/minellama.py
@@ -29,6 +29,7 @@ class Minellama:
         hf_auth_token: str = "", # Hugging face auth token.
         record_file: str = "./log.txt", # the output file to record the result.
         rag_switch = True,
+        search_switch = False,
     ):
         # init env
         self.env = VoyagerEnv(
@@ -41,20 +42,18 @@ class Minellama:
         self.rag_switch=rag_switch
 
         self.llm_model_name=llm_model
-        self.non_RAG_llm = llm_model
-
+        self.search_switch = search_switch
+        
         # set LLM
         if llm == "llama":
             print(f"Llama2 called with rag_switch:{self.rag_switch}")
             self.llm = Llama2(hf_auth_token=hf_auth_token, llm_model=llm_model, local_llm_path=local_llm_path,rag_switch=self.rag_switch)
             
-            self.non_RAG_llm = Llama2(hf_auth_token=hf_auth_token, llm_model=llm_model, local_llm_path=local_llm_path,rag_switch=False)
         elif llm == "gpt":
             print(f"GPT called with rag_switch:{self.rag_switch}")
             os.environ["OPENAI_API_KEY"] = openai_api_key
             self.llm = GPT(llm_model=llm_model,rag_switch=rag_switch)
             
-            self.non_RAG_llm = GPT(llm_model=llm_model,rag_switch=False)
         else:
             # This is for baseline without LLM
             raise ValueError("No LLM selected.")
@@ -62,7 +61,7 @@ class Minellama:
         self.func_list=["craft", "mine", "smelt", "collect", "kill", "fish", "tillAndPlant", "harvest"]    
 
         #* RAGを使わないエージェントを追加
-        self.recipe_agent = RecipeAgent(llm=self.llm, non_RAG_llm=self.non_RAG_llm)
+        self.recipe_agent = RecipeAgent(llm=self.llm, search_switch=self.search_switch)
         # self.recipe_agent = RecipeAgent(llm=self.llm)
         self.action_agent = ActionAgent(llm=self.llm)
         self.role_agent = RoleAgent(llm=self.llm, func_list=self.func_list)
