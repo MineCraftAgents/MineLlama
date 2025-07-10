9d169fbae81f612216f3f2fceec3d2535f4a5b1c
wa-taiki <165888638+wa-taiki@users.noreply.github.com>
2025-07-10 09:08:33 +0900

    Update craftFurnace.js

--- minellama/control_primitives/craftFurnace.js
+++ minellama/control_primitives/craftFurnace.js
@@ -1,4 +1,4 @@
-async function rev_craftFurnace(bot) {
+async function craftFurnace(bot) {
   const furnaceCount = bot.inventory.count(mcData.itemsByName.furnace.id);
   await craftCraftingTable(bot);
   
